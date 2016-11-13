edf0d4d0cec7c38a76cb94bc6e60d834a4ddfad7
Andrzej Puzdrowski <andrzej.puzdrowski@nordicsemi.no>
2016-11-13 18:56:06 +0100

    add mathod for finding all dirs with the same name add method for rename dirs from list

--- dir_manipul.py
+++ dir_manipul.py
@@ -44,6 +44,24 @@ class DirectoryListTool (list):
         
         for i in range(0, len(list_of_idx_subpaths)):
             list_of_pathes.pop(list_of_idx_subpaths[i])
+
+
+    def find_dirs_by_name(self, path, dir_name):
+        self.clear()
+        for root, dirs, files in os.walk(path):
+            for name in dirs:
+                if name == dir_name:
+                    full_path = os.path.join(root, name)
+                    self.append(full_path)
+
+    def rename_dirs(self, new_name , list_of_pathes = None):
+        if list_of_pathes == None:
+            list_of_pathes = self
+
+        for dir_path in list_of_pathes:
+            new_dir_path = os.path.join(os.path.dirname(dir_path), new_name)
+            os.rename(dir_path, new_dir_path)
+
         
     
 # subpathes_helper = subpathes_opertion()
@@ -52,12 +70,18 @@ class DirectoryListTool (list):
 # subpathes_helper.remove_subpathes(test_pathes)
 # print(test_pathes)
 
-mouobj = DirectoryListTool(['mule\\krolek',
-'glue',
-'glue\\def\\z2ylc',
-'mule',
-'mule\\orek'])
-mouobj.append('gryj\\jen\\ol')
+#mouobj = DirectoryListTool(['mule\\krolek',
+#'glue',
+#'glue\\def\\z2ylc',
+#'mule',
+#'mule\\orek'])
+#mouobj.append('gryj\\jen\\ol')
 
-mouobj.remove_subpathes()
+#mouobj.remove_subpathes()
+#print(mouobj)
+
+mouobj = DirectoryListTool()
+mouobj.find_dirs_by_name('G:\\Roboczy\\Andrzej_P\\mbed-os\\mbed\\hal\\targets\\hal\\TARGET_NORDIC\\TARGET_NRF5', 'Ssdk')
+#print(mouobj)
+mouobj.rename_dirs('sdk')
 print(mouobj)
